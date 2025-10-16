21742cbb31a24d64e0b8fd6473c0925e6ab33b02
Jonathan-vaca <Jchaparrovaca@gmail.com>
2025-10-16 16:04:51 -0500

    Sirvo carpeta components para imágenes

--- Backend/server.js
+++ Backend/server.js
@@ -32,7 +32,7 @@ app.use("/api/posts", postsRoutes);
 app.use("/api/posts", commentsRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/contact", contactRoutes);
-app.use("/api/gallery", galleryRoutes);
+app.use("./api/gallery", galleryRoutes);
 
 // ===== Ruta raíz =====
 app.get("/", (req, res) => {
