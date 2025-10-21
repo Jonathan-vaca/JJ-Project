93e466d723934aa9c0914227d898ad94904b1c28
Jonathan-vaca <Jchaparrovaca@gmail.com>
2025-10-21 05:21:27 -0500

    Sirvo carpeta components para imágenes

--- JS/imagen.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const topImageContainer = document.querySelector('.top-image-container');
-    setTimeout(() => {
-        topImageContainer.classList.add('show');
-    }, 100);
-});
